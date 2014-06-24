0c802f4879747ec7ca1ac377953dbd813b25601c
tcnksm <nsd22843@gmail.com>
2014-06-24 18:34:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+homebrew-yo
+====
+
+A [Homebrew](http://brew.sh/) formula for [yo.go](https://github.com/tcnksm/yo).
---
+++ yo.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+class Yo < Formula
+  homepage "https://github.com/tcnksm/yo"
+  version '0.1.0'
+
+  if Hardware.is_64_bit?
+    url "https://github.com/tcnksm/yo/releases/download/v0.1.0/yo_0.1.0_darwin_amd64.zip"
+    sha1 "cf5201a994d9027654e1262a81658ed364aedc5a"
+  else
+    url "https://github.com/tcnksm/yo/releases/download/v0.1.0/yo_0.1.0_darwin_386.zip"
+    sha1 "db4db47eb203b8cf1c9d8e765e4feafa578f108a"
+  end
+
+  depends_on :arch => :intel
+
+  def install
+    bin.install 'yo'
+  end
+end
